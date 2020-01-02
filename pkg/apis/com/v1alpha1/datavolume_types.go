7578d77eaee53e5a29025d1cc582105b398d0c55
John Wass <wassj@ctc.com>
2020-01-01 21:40:40 -0500

    add DataVolume type

---
+++ pkg/apis/com/v1alpha1/datavolume_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DataVolumeSpec defines the desired state of DataVolume
+type DataVolumeSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// DataVolumeStatus defines the observed state of DataVolume
+type DataVolumeStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// DataVolume is the Schema for the datavolumes API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=datavolumes,scope=Namespaced
+type DataVolume struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DataVolumeSpec   `json:"spec,omitempty"`
+	Status DataVolumeStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// DataVolumeList contains a list of DataVolume
+type DataVolumeList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []DataVolume `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&DataVolume{}, &DataVolumeList{})
+}
